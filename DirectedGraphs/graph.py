74da976099c615f632da3bde594b44cf3c3a020d
Israel Campiotti <israelcampiotti@gmail.com>
2018-04-18 21:47:54 -0300

    Add files via upload

--- DirectedGraphs/graph.py
+++ DirectedGraphs/graph.py
@@ -1,54 +1,202 @@
 
-class Graph():
+class Node():
+
+
+    def __init__(self, name):
+        """Class for a node in a Directed Graph."""
+        self.name = name
+        self.connections = []
+        self.costs = []
+        self.count = 0
+
+
+    def AddConnection(self, node, cost):
+        """Adds a connection to the node."""
+        self.connections.append(node)
+        self.costs.append(int(cost))
+
+    def SetDistance(self, dist):
+        """Set the distance for a node."""
+        self.distance = dist
+
+    def SetPred(self, pred):
+        """Set predecessor for a node."""
+        self.pred = pred
+
+    def AddCount(self):
+        """Adds one to variable count."""
+        self.count += 1
+
+    def SetLabel(self, label):
+        """Sets the label of the node."""
+        self.label = label
+
+class DirectGraph():
     def __init__(self, graph):
-        self.graph = dict()
-        self.CreateGraph(graph)
+        """Class for a Directed Graph."""
+        self.graph = graph
+        self._CreateArcs()
 
-    #adds connection to the graph
-    def CreateGraph(self, graph):
-        for i in range(len(graph)):
-            self.AddConection(graph[i][0], graph[i][1], graph[i][2])
+    def PrintsGraph(self):
+        """Prints nodes, arcs and costs."""
+        for no in self._NodeGenerator():
+            print('Name {}, Connected to {} with costs {}'.format(no.name, no.connections, no.costs))
 
-    def AddConection(self, nodeA, nodeB, cost):
-        self.graph.setdefault(nodeA, []).append([nodeB, cost])
-        self.graph.setdefault(nodeB, []).append([nodeA, cost])
+    def PrintShortPath(self):
+        """Prints the shortest path found with FIFO or Dijkstra."""
+        for key in self.nodes:
+            print('Node {} has predecessor {}'.format(key, self.nodes[key].pred))
 
-    #given the set of nodes S, returns the archs and the costs of the possible archs
-    #the possible archs are the ones that conect nodes in S to nodes not in S
-    def FindArchs(self, S):
-        archs, costs = [], []
-        for node in S:
-            info = self.graph[node]
-            for i in range(len(info)):
-                if info[i][0] not in S:
-                    archs.append([node, info[i][0]])
-                    costs.append(info[i][1])
-        return archs, costs
+    def Dijkstra(self, initial_node='A'):
+        """Performs Dijkstra algorithm."""
+        #initiating the variables
+        S = []
+        for node in self._NodeGenerator():
+            node.SetDistance(100000000)    
+        self.nodes[initial_node].SetDistance(0)
+        self.nodes[initial_node].SetPred(0)
+        while len(S) < len(self.nodes):
+            #create complementary to S node set
+            S_line = self._DefineS_line(S)
+            #chooses node with least distance
+            node_i = self._LeastDistanceNode(S_line)
+            #updates the distance from adjacent nodes
+            self._UpdateAdjacentNodes(node_i)
+            #adds node to list
+            S.append(node_i)
 
-    #find the arch with lowest cost in the given set
-    def FindLowestArch(self, Archs, Costs):
-        index = Costs.index(min(Costs))
-        return Archs[index], Costs[index]
-
-    #gives the minimum spanning tree using primms algorithm
-    def SolvePrimm(self, initial_node):
-        #declare tree, costs and S
-        tree = []
-        costs = []
+    def FIFO(self, initial_node='A'):
+        """Performs the FIFO algorithm"""
+        for key in self.nodes:
+            self.nodes[key].SetDistance(100000000)    
+        self.nodes[initial_node].SetDistance(0)
+        self.nodes[initial_node].SetPred(0)
         S = [initial_node]
-        #check stop condition
-        while len(S) != len(self.graph):
-            #find possible archs and their costs
-            Archs, Costs = self.FindArchs(S)
-            #select the arch with lowest cost
-            LowestArch, LowestCost = self.FindLowestArch(Archs, Costs)
-            #append the arch to tree and saves the cost
-            tree.append(LowestArch)
-            costs.append(LowestCost)
-            #update the set of nodes S
-            S.append(LowestArch[1])
-        return tree, costs
+        while len(S) > 0:
+            node_i = S[0]
+            L = self._UpdateAdjacentNodes(node_i, FIFO=True)
+            for node in L:  
+                if node not in S:   
+                    S.append(node)
+            #update the counts
+            self._UpdateCounts(S)
+            #checks for negative cycle
+            if self._NegativeCycle(len(self.nodes)):
+                raise ValueError('Algorithm terminated! Negative Cycle detected!')
+            S.remove(node_i)
 
-    #returns the nodes in the graph
-    def GetNodes(self):
-        return [key for key in self.graph]
+    def Floyd(self):
+        """Performs Floyd All-Pairs algorithm"""
+        self._SetNodesLabels()
+        disMatrix, predMatrix = self._FloydMatrices()
+        C = self._FindUpperBound()
+        print(C)
+        n = len(disMatrix)
+        for k in range(n):
+            for i in range(n):
+                for j in range(n):
+                    if disMatrix[i][j] > disMatrix[i][k] + disMatrix[k][j] and disMatrix[i][k] < n*C:
+                        disMatrix[i][j] = disMatrix[i][k] + disMatrix[k][j]
+                        predMatrix[i][j] = predMatrix[k][j]
+                #detects negative cycle        
+                if disMatrix[i][i] < 0:
+                    raise ValueError('Algorithm terminated! Negative Cycle detected!')
+            #detects negative cycle
+            if disMatrix.min() < -n*C:
+                raise ValueError('Algorithm terminated! Negative Cycle detected!')
+        print('---Distance Matrix---')
+        print(disMatrix)
+        print('---Predecessors Matrix---')
+        print(predMatrix)
+
+    def _CreateArcs(self):
+        """Creates the arcs of the graph"""
+        self.nodes = {}
+        for arc in self.graph:
+            if arc[0] not in self.nodes:
+                self.nodes[arc[0]] = Node(arc[0])
+            if arc[1] not in self.nodes:
+                self.nodes[arc[1]] = Node(arc[1])
+            self.nodes[arc[0]].AddConnection(self.nodes[arc[1]].name, cost=arc[2])
+
+    def _NodeGenerator(self):
+        """Generator that returns the node classes"""
+        for key in self.nodes:
+            yield self.nodes[key]
+
+    def _DefineS_line(self, S):
+        """given a set of nodes S defines the complementary"""
+        S_line = []
+        for node_name in self.nodes:
+            if node_name not in S:
+                S_line.append(node_name)
+        return S_line
+
+    def _LeastDistanceNode(self, S):
+        """given a set S of nodes return the node with the least distance"""
+        least_node = 0
+        if len(S) > 0:
+            least_node = S[0]
+            for node_name in S:
+                if self.nodes[node_name].distance < self.nodes[least_node].distance:
+                    least_node = node_name
+        return least_node
+
+    def _UpdateAdjacentNodes(self, current_node, FIFO=False):
+        """Updates adjacent nodes for the Dijkstra or FIFO algorithm"""
+        FIFO_list = []
+        node_c = self.nodes[current_node]
+        for i in range(len(node_c.connections)):
+            node_i = node_c.connections[i]
+            if self.nodes[node_i].distance > node_c.distance + node_c.costs[i]:
+                self.nodes[node_i].SetDistance(node_c.distance + node_c.costs[i])
+                self.nodes[node_i].SetPred(current_node)
+                if FIFO:
+                    FIFO_list.append(node_i)
+        if FIFO:
+            return FIFO_list
+
+    def _UpdateCounts(self, S):
+        """Update the counts for every node, used in FIFO"""
+        for node in S:
+            self.nodes[node].AddCount()
+
+    def _NegativeCycle(self, n):
+        """Checks if the count surpass maximum value n. used in FIFO"""
+        for node in self._NodeGenerator():
+            if node.count > n:
+                return True
+        return False
+
+    def _SetNodesLabels(self):
+        """Set labels for every node, used in Floyd"""
+        i = 0
+        for node in self._NodeGenerator():
+            node.SetLabel(i)
+            i += 1
+
+    def _FloydMatrices(self):
+        """Creates the initial matrices for Floyd"""
+        import numpy as np
+        dim = len(self.nodes)
+        D = np.inf*np.ones((dim, dim))
+        P = np.chararray((dim, dim))
+        P[:] = '-'
+        #set 0 on the diagonal
+        for i in range(dim):
+            D[i][i] = 0
+        #seting the arcs that already exists
+        for node in self._NodeGenerator():
+            for i in range(len(node.connections)):
+                D[node.label][self.nodes[node.connections[i]].label] = node.costs[i]
+                P[node.label][self.nodes[node.connections[i]].label] = node.name
+        return D, P
+
+    def _FindUpperBound(self):
+        """Given a network find the upper bound for the costs, used in Floyd"""
+        C = -10000
+        for node in self._NodeGenerator():
+            if len(node.costs) > 0:
+                if C < max(node.costs):
+                    C = max(node.costs)
+        return C + 10
--- DirectedGraphs/main.py
+++ DirectedGraphs/main.py
@@ -1,13 +1,6 @@
 import argparse
-import pyglet
-from pyglet import gl
-from graph import Graph
-from glGraph import glGraph
 
-#creates window
-width, height = 600, 400
-window = pyglet.window.Window(width, height, caption='Primms Algorithm', resizable=True)
-gl.glClearColor(1.0, 1.0, 1.0, 1.0)
+from graph import DirectGraph
 
 #receiving the file with the archs
 parser = argparse.ArgumentParser()
@@ -19,38 +12,31 @@ def remove_vals(lista, val):
     for i in range(len(lista)):
         while val in lista[i]: lista[i].remove(val)
     return lista
-
+def find_negatives(lista):
+    for arc in lista:
+        if len(arc) > 3:
+            if '-' in arc:
+                arc.remove('-')
+                arc[2] = -int(arc[2])
+            else:
+                arc[2] = "".join(arc[2:])
+                arc.pop(3)
+    return lista
 #reading from the file
 file = open(name_file, 'r')
 graph = []
 for line in file:
     graph.append(list(line))
-graph = remove_vals(graph, ' ')
 graph = remove_vals(graph, '\n')
+graph = find_negatives(graph)
+print(graph)
 
-#creating the graph
-G = glGraph(graph)
-# G.CreateGraph(graph)
-# tree, costs = G.SolvePrimm(initial_node=graph[0][0])
-# print(tree, costs)
-
-#get nodes
-nodes = G.GetNodes()
-print(nodes)
-
-# label = pyglet.text.Label('A',font_name='Times New Roman',font_size=100,x=200, y=200, color=(0,0,0,255))
-@window.event
-def on_draw():
-    gl.glPushMatrix()
-    gl.glViewport(0, 0, window.width, window.height)
-    gl.glMatrixMode(gl.GL_PROJECTION)
-    gl.glLoadIdentity()
-    gl.glOrtho(0, width, 0, height, -1, 1)
-    gl.glMatrixMode(gl.GL_MODELVIEW)
-    #
-    window.clear()
-    G.ShowNodes()
-    #
-    gl.glPopMatrix()
-
-pyglet.app.run()
+dg = DirectGraph(graph)
+dg.PrintsGraph()
+# dg.Dijkstra()
+# print('---Dijkstra---')
+# dg.PrintShortPath()
+# dg.FIFO()
+# print('---FIFO---')
+# dg.PrintShortPath()
+dg.Floyd()
